6c6b74ab4486d0ab14a23dd0f0884a1675ffb039
Tomer Levy <tomerlv104@gmail.com>
2026-01-04 05:33:04 +0200

    Initial Server

---
+++ src/test/java/com/surveysdk/server/SurveySdkServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surveysdk.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveySdkServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
