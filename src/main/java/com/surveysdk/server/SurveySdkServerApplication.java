5a3c2a3aed5b58f1c5ac50d87451fa5ab182739f
Tomer Levy <tomerlv104@gmail.com>
2026-01-12 03:10:13 +0200

    refactor: SpirngBoot framework added

--- src/main/java/com/surveysdk/server/SurveySdkServerApplication.java
+++ src/main/java/com/surveysdk/server/SurveySdkServerApplication.java
@@ -2,11 +2,14 @@ package com.surveysdk.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing // Identifies fields with @CreatedDate and fill them automatically.
 public class SurveySdkServerApplication {
 
     public static void main(String[] args) {
+        System.out.println("MONGODB_URI from env = " + System.getenv("MONGODB_URI"));
         SpringApplication.run(SurveySdkServerApplication.class, args);
     }
 
