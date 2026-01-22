c663da8d440b57b1e32ccdf1a917fa06918ffeae
Tomer Levy <tomerlv104@gmail.com>
2026-01-22 20:25:29 +0200

    General: Some updates for Render

--- src/main/java/com/surveysdk/server/utils/cors/CorsConfig.java
+++ src/main/java/com/surveysdk/server/utils/cors/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://survey-admin-panel.onrender.com","http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
